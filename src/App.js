37418617c543785c6c41c2e95deb6174bed4ebb6
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-03-05 15:02:04 +0100

    commit before merge

--- src/App.js
+++ src/App.js
@@ -50,7 +50,9 @@ class App extends Component {
     let email = event.target.email.value;
     let password = event.target.password.value;
 
-    axios.post(`${config.API_URL}/api/signin`, {   //which api url goes here? config is not defined?
+    axios
+      .post(`${config.API_URL}/api/signin`, {
+        //which api url goes here? config is not defined?
         email: email,
         password: password,
       })
@@ -69,12 +71,58 @@ class App extends Component {
       });
   };
 
+  //Create story
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let location = event.target.location.value;
+    let image = event.target.image.files[0];
+    let title = event.target.title.value;
+    let description = event.target.description.value;
+    // let public = event.target.public.value
+
+    let uploadForm = new FormData();
+    uploadForm.append("imageUrl", image);
+
+    axios
+      .post(`${config.API_Url}/api/upload`, uploadForm)
+
+      .then((response) => {
+        // make an API call to the server side route to create a new story
+        axios
+          .post(`${config.API_URL}/api/create`, {
+            location: location,
+            image: response.data.image,
+            title: title,
+            description: description,
+            // public: public,
+            completed: false,
+          })
+
+          .then((response) => {
+            // when the server has created a story
+            // update the state that is visible to the user
+            this.setState(
+              {
+                stories: [response.data, ...this.state.stories],
+              },
+              () => {
+                // when story is created lead user to his page
+                this.props.history.push("/api/user/:userId");
+              }
+            );
+          })
+          .catch((error) => {
+            console.log("Creating story failed", error);
+          });
+      });
+  };
+
   render() {
     return (
       <div>
-      <NavTop />
-      <CreateStory />
-      
+        <NavTop />
+        {/* <CreateStory /> */}
+
         <Switch>
           <Route exact path="/Homepage" component={Homepage} />
           <Route
@@ -84,10 +132,17 @@ class App extends Component {
               return <Signup onAdd={this.handleSignup} />; //can we have onAdd in both signup and login cases.
             }}
           />
-          <Route exact path="/login" render={() => {
+          <Route
+            exact
+            path="/login"
+            render={() => {
               return <Login onAdd={this.handleLogin} />; //would onAdd work???
             }}
           />
+           <Route path="/create" render={() => {
+                return <CreateStory onCreate={this.handleSubmit} />
+           }} /> 
+
         </Switch>
       </div>
     );
--- src/components/CreateStory.js
+++ src/components/CreateStory.js
@@ -14,52 +14,52 @@ import config from '../config'
 
     }
 
-    handleSubmit =(event) => {
-        event.preventDefault() 
-            let location = event.target.location.value
-            let image = event.target.image.files[0]
-            let title = event.target.title.value
-            let description = event.target.description.value
-            // let public = event.target.public.value
+    // handleSubmit =(event) => {
+    //     event.preventDefault() 
+    //         let location = event.target.location.value
+    //         let image = event.target.image.files[0]
+    //         let title = event.target.title.value
+    //         let description = event.target.description.value
+    //         // let public = event.target.public.value
             
         
 
-        let uploadForm = new FormData()
-        uploadForm.append('imageUrl', image)
+    //     let uploadForm = new FormData()
+    //     uploadForm.append('imageUrl', image)
 
-        // send image to cloudinary
-        axios.post(`${config.API_Url}/api/upload`, uploadForm)
+    //     // send image to cloudinary
+    //     axios.post(`${config.API_Url}/api/upload`, uploadForm)
             
-        .then((response) => {
-            // make an API call to the server side route to create a new story
-            axios.post(`${config.API_URL}/api/create`, {
+    //     .then((response) => {
+    //         // make an API call to the server side route to create a new story
+    //         axios.post(`${config.API_URL}/api/create`, {
 
-                location: location,
-                image: response.data.image,
-                title: title,
-                description: description,
-                // public: public,
-                completed: false
+    //             location: location,
+    //             image: response.data.image,
+    //             title: title,
+    //             description: description,
+    //             // public: public,
+    //             completed: false
 
-            })
+    //         })
 
-        .then((response) => {
-            // when the server has created a story
-            // update the state that is visible to the user
-            this.setState({
-                stories: [response.data, ...this.state.stories]
-            }, () => {
-            // when story is created lead user to his page
-                this.props.history.push('/api/user/:userId')
-            })
-        })
-        .catch((error) => {
-            console.log("Creating story failed", error)
-        })        
+    //     .then((response) => {
+    //         // when the server has created a story
+    //         // update the state that is visible to the user
+    //         this.setState({
+    //             stories: [response.data, ...this.state.stories]
+    //         }, () => {
+    //         // when story is created lead user to his page
+    //             this.props.history.push('/api/user/:userId')
+    //         })
+    //     })
+    //     .catch((error) => {
+    //         console.log("Creating story failed", error)
+    //     })        
 
-        })
+    //     })
 
-    }
+    // }
 
     render() {
         return (
@@ -83,9 +83,7 @@ import config from '../config'
                    
                 </Snuggle>
 
-            //      {/* <Route path="/api/create" render={() => {
-            //     return <CreateStory onCreate={this.handleSubmit} />
-            // }} /> */}
+            
 
                     
         )
