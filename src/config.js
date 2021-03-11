cddffa1016495820634f0024c5a126e2180d7867
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-03-11 19:26:33 +0100

    before merge commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_URL: process.env.REACT_APP_SERVER_URL,
   GEO_URL:
-    "http://api.positionstack.com/v1/forward?access_key=dd44dfea77b439fad7a76f83765bc8d5&query=", //if we go over 25.000 req make another acc and paste key here
+    "https://delia-aleks-proxy.herokuapp.com/pipe/http://api.positionstack.com/v1/forward?access_key=dd44dfea77b439fad7a76f83765bc8d5&query=", //if we go over 25.000 req make another acc and paste key here
 };
